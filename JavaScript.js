6bc873397bd58c658527025bb192a7ebbff8c09a
Thomas <Thomas.Face03@gmail.com>
2016-12-17 13:32:46 +0100

    Test JS Var & Switch

---
+++ JavaScript.js
@@ -0,0 +1,17 @@
+var jeux = prompt ("Quel est votre jeux vidéo préféré ?");
+switch (jeux)
+
+{
+	case "Dofus" :
+	alert("La base du MMORPG");
+	break;
+	case "Minecraft" :
+	alert("Très bon jeux...");
+	break;
+	case "Fifa" :
+	alert("Bien, bien..");
+	break;
+	default :
+	alert("Je ne connais pas ce jeux...");
+	break;
+}
